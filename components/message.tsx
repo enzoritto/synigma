2900cf2065fe9529959e689cc0940f9bb9b1dbc8
Enzo Cranford <enzo.cranford@gmail.com>
2023-10-22 16:55:35 -0600

    feat: add reaction messages

---
+++ components/message.tsx
@@ -0,0 +1,24 @@
+import { useEffect } from 'preact/hooks';
+
+export default function Message({ message, isVisible, toggleVisibility }) {
+  useEffect(() => {
+    if (isVisible) {
+      const timer = setTimeout(() => {
+        toggleVisibility();
+      }, 3000);
+
+      return () => {
+        clearTimeout(timer);
+      };
+    }
+  }, [isVisible, toggleVisibility]);
+
+  return (
+    isVisible && (
+      <div>
+        {message}
+      </div>
+    )
+  );
+}
+
--- islands/game.tsx
+++ islands/game.tsx
@@ -1,11 +1,16 @@
 import { selectRandom, getAvailableSynonyms } from "../lib/main.ts";
-import { useState } from 'preact/hooks';
+import { useState, useEffect } from 'preact/hooks';
+import Message from '../components/message.tsx';
 
 export default function Game({ dictionary, answer, startingSynonym }) {
   const [currentSynonyms, setCurrentSynonyms] = useState<string[]>([startingSynonym]);
   const [availableSynonyms, setAvailableSynonyms] = useState<string[]>(getAvailableSynonyms(answer.synonyms, currentSynonyms));
   const [guess, setGuess] = useState<string>('');
+
   const [gameOver, setGameOver] = useState<boolean>(false);
+
+  const [message, setMessage] = useState<string>('');
+  const [showMessage, setShowMessage] = useState<boolean>(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -13,13 +18,13 @@ export default function Game({ dictionary, answer, startingSynonym }) {
     if (!guess) return;
 
     if (currentSynonyms.includes(guess)) {
-      alert('Invalid guess!');
+      setMessage('Invalid guess!')
       setGuess('');
       return;
     }
 
     if (guess === answer.word) {
-      alert('Correct!');
+      setMessage('Correct! You won!')
       setGameOver(true);
       return;
     }
@@ -30,13 +35,14 @@ export default function Game({ dictionary, answer, startingSynonym }) {
 
       setCurrentSynonyms(updatedCurrentSynonyms);
       setAvailableSynonyms(updatedAvailableSynonyms);
+      setMessage("You're on the right track!");
       setGuess('');
     } else if (availableSynonyms.length === 0) {
-      alert('You lost!');
+      setMessage(`You lost! The word was ${answer.word}`);
       setGameOver(true);
       return;
     } else {
-      alert('Wrong!');
+      setMessage('Wrong! Try again!');
       const randomAvailableSynonym = selectRandom(availableSynonyms);
       const updatedCurrentSynonyms = [...currentSynonyms, randomAvailableSynonym];
       const updatedAvailableSynonyms = availableSynonyms.filter(synonym => synonym !== randomAvailableSynonym);
@@ -47,6 +53,14 @@ export default function Game({ dictionary, answer, startingSynonym }) {
     }
   };
 
+  useEffect(() => {
+    setShowMessage(true);
+  }, [message])
+
+  const hideMessage = () => {
+    setShowMessage(false);
+  };
+
   const handleInputChange = (e) => {
     setGuess(e.target.value);
   };
@@ -54,6 +68,7 @@ export default function Game({ dictionary, answer, startingSynonym }) {
   return (
     <div>
       <p>Current Synonyms: <strong>{currentSynonyms.join(', ')}</strong></p>
+
       <form onSubmit={handleSubmit}>
         <input
           type="text"
@@ -67,6 +82,14 @@ export default function Game({ dictionary, answer, startingSynonym }) {
           disabled={gameOver && 'disabled'}
         >Check Guess</button>
       </form>
+
+      {showMessage && (
+        <Message
+          message={message}
+          isVisible={showMessage}
+          toggleVisibility={hideMessage}
+        />
+      )}
     </div>
   );
 }
