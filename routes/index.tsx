42d8d2b4c7f7518adf0897a9fb1669ba332f24d7
Enzo Cranford <enzo.cranford@gmail.com>
2023-10-22 02:29:24 -0600

    feat: load synonym dictionary

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,7 +1,21 @@
-export default function Home() {
+export type Word = {
+  "word": string;
+  "synonyms": string[];
+}
+
+async function getDictionary(): Word[] {
+  const plainText: string = await Deno.readTextFile("static/synonyms.jsonl");
+  const unparsedDictionary: string[] = plainText.split(/\n/g);
+  return await unparsedDictionary.map((line) => JSON.parse(line));
+}
+
+export default async function Home() {
+  const dictionary: Word[] = await getDictionary();
+
   return (
     <div>
       <h1>Synigma</h1>
+      {dictionary.map(word => <p>{word.word}</p>)}
     </div>
   );
 }
