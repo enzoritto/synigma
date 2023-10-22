c25e0d77e3eb1dbfdbaf02da06095628673b7f83
Enzo Cranford <enzo.cranford@gmail.com>
2023-10-22 13:43:39 -0600

    feat: select random synonym

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,12 +1,16 @@
 import { getDictionary, selectRandom } from "../lib/main.ts";
 
+
 export default async function Home() {
   const dictionary: Word[] = await getDictionary();
   const answer: Word = selectRandom(dictionary);
+  const currentSynonyms: string[] = [selectRandom(answer.synonyms)];
 
   return (
     <div>
       <h1>The answer is {answer.word}</h1>
+      <h2>The synonyms are: {currentSynonyms}</h2>
     </div>
   );
 }
+
