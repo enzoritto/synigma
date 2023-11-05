3fd6bed2be3a7cbae377fbcae83309ebd88ba21f
Enzo Cranford <enzo.cranford@gmail.com>
2023-11-05 16:59:19 -0700

    chore: add lang attribute to html tag

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -3,7 +3,7 @@ import { asset } from "$fresh/runtime.ts";
 
 export default function App({ Component }: AppProps) {
   return (
-    <html>
+    <html lang="en">
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
