18793bd5c809d09fc59e7783feaa9f3e0aaf1d15
Enzo Cranford <enzo.cranford@gmail.com>
2023-11-05 16:50:34 -0700

    chore: wrap style.css url with asset function call

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from "$fresh/server.ts";
+import { asset } from "$fresh/runtime.ts";
 
 export default function App({ Component }: AppProps) {
   return (
@@ -7,7 +8,7 @@ export default function App({ Component }: AppProps) {
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Synigma</title>
-        <link rel="stylesheet" href="/styles.css" />
+        <link rel="stylesheet" href={asset("/styles.css")} />
       </head>
       <body>
         <Component />
